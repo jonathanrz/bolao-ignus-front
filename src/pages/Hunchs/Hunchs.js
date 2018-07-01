e23f8e2979315367ea39d61dd0f5461c56af5177
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-07-01 20:02:04 -0300

    Points

--- src/pages/Hunchs/Hunchs.js
+++ src/pages/Hunchs/Hunchs.js
@@ -1,24 +1,43 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
 import { Query } from 'react-apollo'
 import { sortBy, find } from 'lodash'
 
 import Loader from 'app/components/Loader'
 import matchQuery from 'app/queries/Matchs'
 import hunchQuery from 'app/queries/Hunchs'
+import pointsQuery from 'app/queries/HunchPoints'
 
 import Form from './Form'
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 50px;
+`
 
 function Hunchs({ matchs, hunchs, createHunch }) {
   return (
     <Fragment>
-      {matchs.map(match => (
-        <Form
-          key={match.id}
-          match={match}
-          hunch={find(hunchs, { match: { id: match.id } })}
-          createHunch={createHunch}
-        />
-      ))}
+      {matchs.map(match => {
+        const hunch = find(hunchs, { match: { id: match.id } })
+        return (
+          <Container key={match.id}>
+            <Form match={match} hunch={hunch} createHunch={createHunch} />
+            {hunch && (
+              <Query query={pointsQuery} variables={{ id: hunch.id }}>
+                {({ loading, error, data }) => {
+                  if (loading) return 'Carregando pontos do palpite'
+                  if (error) return null
+                  const { hunchPoints } = data
+                  return hunchPoints.points >= 0 ? (
+                    <div>{`Pontos: ${hunchPoints.points}`}</div>
+                  ) : null
+                }}
+              </Query>
+            )}
+          </Container>
+        )
+      })}
     </Fragment>
   )
 }
